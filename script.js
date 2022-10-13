064235f9c58772620037179331d8d0342359ed2f
Andy Phinney <111609415+aphinney0516@users.noreply.github.com>
2022-10-13 14:54:21 -0500

    Refactored the code.

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function askUser() {
+  // get length and selected character types from user
+  var passLength = parseInt (window.prompt("Please enter your password length."));
+  console.log(passLength);
+  if(passLength === "" || passLength === null || isNaN(passLength) || passLength < 8 || passLength > 128) {
+    window.alert("Please enter a valid password length.");
+    return;
+  }
+  var characterString = "";
+  if(window.confirm("Would you like lowercase characters?")) {
+    // console.log("Add lowercase here.");
+    characterString += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if(window.confirm("Would you like uppercase characters?")) {
+    // console.log("Add lowercase here.");
+    characterString += "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+  }
+  if(window.confirm("Would you like numbers characters?")) {
+    // console.log("Add lowercase here.");
+    characterString += "0123456789";
+  }
+  if(window.confirm("Would you like special characters?")) {
+    // console.log("Add lowercase here.");
+    characterString += "~!@#$%&*()_+";
+  }
+  // console.log(characterString);
+  if(characterString === "") {
+    window.alert("You must select one character type.");
+    return;
+  }
+  return {
+    passLength, characterString
+  }
+}
+
+function generatePassword() {
+  // return a password that matches the legnth and character type that the user selects
+  var options = askUser();
+  if(options === undefined) {
+    return "Please try again."
+  }
+  var myArray = options.characterString.split("");
+  var password = "";
+  for(var i = 0; i < options.passLength; i++) {
+    var index = Math.floor(Math.random() * myArray.length)
+    var digit = myArray[index];
+    password += digit;
+  }
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
